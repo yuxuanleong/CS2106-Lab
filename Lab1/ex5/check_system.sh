bdd7338c4df40395373e5eeb1b8d05b808cbf18e
Timothyoung97 <timothyoung97@gmail.com>
2021-09-03 11:52:38 +0800

    Complete ex5 & ex6

--- Lab1/ex5/check_system.sh
+++ Lab1/ex5/check_system.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-
 ####################
 # Lab 1 Exercise 5
-# Name:
-# Student No:
-# Lab Group: 
+# Name: Yang Shiyuan
+# Student No: A0214269A
+# Lab Group: B17
 ####################
-
 # Fill the below up
 hostname=$(hostname)
-machine_hardware=$(uname --m)
+machine_hardware=$(uname -s -m)
 max_user_process_count=$(cat /proc/sys/kernel/pid_max)
-user_process_count=$(ps -e --no-headers| wc -l)
+user_process_count=$(ps -e --no-headers | wc -l)
 user_with_most_processes=$(ps axho user --sort -rss | head -1)
 mem_free_percentage=$(free | grep Mem | awk '{print $4/$2 * 100.0}')
 swap_free_percentage=$(free | grep Swap | awk '{print $4/$2 * 100.0}')
-
 echo "Hostname: $hostname"
 echo "Machine Hardware: $machine_hardware"
 echo "Max User Processes: $max_user_process_count"
