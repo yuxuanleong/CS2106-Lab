f65028e4d9d7deff6a2f9f1f793aeaa45e6f5641
Timothyoung97 <timothyoung97@gmail.com>
2021-09-03 14:56:19 +0800

    Add comment for ex2

--- Lab1/ex2/node.c
+++ Lab1/ex2/node.c
@@ -16,12 +16,11 @@
 // Feel free to add any headers you deem fit (although you do not need to)
 
 /**
- * What are some of the common function we should have?
- * 1. getNode at the index
- * 2. create new node with data
- * 3. create a empty list
+ * @brief Get the node pt object at a specific index
+ * @param lst 
+ * @param index 
+ * @return node* 
  */
-
 node *get_node_pt(list *lst, int index) {
     
     int curr_index = 0;
@@ -34,6 +33,12 @@ node *get_node_pt(list *lst, int index) {
     return curr_node_pt;
 }
 
+/**
+ * @brief Create a new node pt object with next pointer set as null
+ * 
+ * @param data 
+ * @return node* 
+ */
 node *create_new_node_pt(int data) {
 
     node *new_node_pt = (node *) malloc(sizeof(node));
@@ -42,107 +47,153 @@ node *create_new_node_pt(int data) {
     return new_node_pt;
 }
 
+/**
+ * @brief check if a list is currently empty
+ * 
+ * @param list_pt 
+ * @return int 
+ */
 int is_empty_list(list *list_pt) {
     return list_pt->head == NULL;
 }
 
+/**
+ * @brief remove a node at a specfic index
+ * 
+ * @param prev_pt 
+ * @param curr_pt 
+ */
 void remove_node(node *prev_pt, node *curr_pt) {
     prev_pt->next = curr_pt->next;
     free(curr_pt);
 }
 
+/**
+ * @brief insert a node after a specific index
+ * 
+ * @param this_node 
+ * @param new_node 
+ */
 void insert_node_after(node *this_node, node *new_node) {
 
     new_node->next = this_node->next;
     this_node->next = this_node;
 }
 
-/**
- * Essential things to keep track:
- * How to create a pointer
- * How to allocate memory to a pointer
- */
-
-
-/**
- * Cases:
- * 1. if the list is empty, then we need to create the list and insert the node at zero
- * 2. if the list is not empty, then we need to find the node specify by the index,
- *      then get that node to point to the newNode, and then set the newNode to point to the next node
- * 3. if the node to insert is at the last place, then we need to point the newNode to the first node's address 
- */
-
 // Inserts a new node with data value at index (counting from head
 // starting at 0).
 // Note: index is guaranteed to be valid.
+
+/**
+ * @brief insert a node at a specific index
+ * 
+ * @param lst 
+ * @param index 
+ * @param data 
+ */
 void insert_node_at(list *lst, int index, int data) {
 
+    // create the new node
     node *new_node_pt_at_index = create_new_node_pt(data);
 
+    // if the list is empty, we initialize the list
     if (is_empty_list(lst)) {
         lst->head = new_node_pt_at_index;
         new_node_pt_at_index->next = lst->head;
         return;
     }
 
+    // if we are going to insert at 0, else we insert at any index
     if (!index) {
+        
         node* temp = lst->head;
         new_node_pt_at_index->next = temp;
 
+        // iterate to end of the list
         while (temp->next != lst->head) {
             temp = temp->next;
         }
 
+        // adjust the pointer at the end of list
         lst->head = new_node_pt_at_index;
         temp->next = new_node_pt_at_index;
         return;
     } else {
+
         int curr_index = 0;
         node* temp = lst->head;
+
+        // iterate to end of the list
         while (curr_index != index - 1) {
             temp = temp->next;
             curr_index++;
         }
+
+        // adjust the node
         new_node_pt_at_index->next = temp->next;
         temp->next = new_node_pt_at_index;
+        
         return;
     }
 }
 
 // Deletes node at index (counting from head starting from 0).
 // Note: index is guarenteed to be valid.
+/**
+ * @brief delete a node from the list
+ * 
+ * @param lst 
+ * @param index 
+ */
 void delete_node_at(list *lst, int index) {
     
+    // if the list is already empty, then we just prompt the user
     if (is_empty_list(lst)) {
         printf("The list is empty");
         return;
     }
 
+    // if the node we are going to delete is the last node in the list
     if (lst->head == lst->head->next) {
         free(lst->head);
         lst->head = NULL;
         return;
     }
 
+    // if we are going to remove a node from the start, else we remove a node from any index
     if (!index) {
+
         node* front = lst->head;
         node* temp = lst->head;
+        
+        // iterate to the end the list
         while (temp->next != lst->head) {
             temp = temp->next;
         }
+
+        //  adjustment of the list
         lst->head = lst->head->next;
         temp->next = lst->head;
+        //  free the front
         free(front);
+
         return;
     } else {
+
         int curr_index = 0;
         node* temp = lst->head;
+
+        // find the node at the index
         while (curr_index != index - 1) {
             temp = temp->next;
             curr_index++;
         }
+
+        // adjustment of the list
         node* node_to_delete = temp->next;
         temp->next = temp->next->next;
+
+        // free the node
         free(node_to_delete);
         return;
     }
@@ -164,6 +215,7 @@ void rotate_list(list *lst, int offset) {
 // becoming the new head node.
 void reverse_list(list *lst) {
     
+    // if list is empty, we just prompt the user
     if (is_empty_list(lst)) {
         printf("list empty");
         return;
@@ -187,17 +239,20 @@ void reverse_list(list *lst) {
 // any allocated memory in the process
 void reset_list(list *lst) {
 
+    // empty list
     if (is_empty_list(lst)) {
         lst->head = NULL;
         return;
     }
 
+    // only one node in list
     if (lst->head == lst->head->next) {
         free(lst->head);
         lst->head = NULL;
         return;
     }
 
+    // iteratively free all nodes
     node* curr = lst->head;
     while (curr->next != lst->head) {
         node* prev = curr; 
