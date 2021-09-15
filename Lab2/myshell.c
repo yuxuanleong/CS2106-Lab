bf88635f9e4fdeb7ff79e83831edfc7b379a498f
Timothyoung97 <timothyoung97@gmail.com>
2021-09-15 11:29:56 +0800

    Add Lab2 docs

---
+++ Lab2/myshell.c
@@ -0,0 +1,21 @@
+/**
+ * CS2106 AY21/22 Semester 1 - Lab 2
+ *
+ * This file contains function definitions. Your implementation should go in
+ * this file.
+ */
+
+#include "myshell.h"
+
+
+void my_init(void) {
+    // Initialize what you need here
+}
+
+void my_process_command(size_t num_tokens, char **tokens) {
+    // Your code here, refer to the lab document for a description of the arguments
+}
+
+void my_quit(void) {
+    // Clean up function, called after "quit" is entered as a user command
+}
