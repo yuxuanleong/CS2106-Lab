a736e991772b58e7fdb87be167edd00ba1cd9412
Timothyoung97 <timothyoung97@gmail.com>
2021-11-10 13:23:14 +0800

    Upload Lab 4 files

---
+++ Lab4/userswap.c
@@ -0,0 +1,17 @@
+#include "userswap.h"
+
+void userswap_set_size(size_t size) {
+
+}
+
+void *userswap_alloc(size_t size) {
+  return NULL;
+}
+
+void userswap_free(void *mem) {
+
+}
+
+void *userswap_map(int fd, size_t size) {
+  return NULL;
+}
